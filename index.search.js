79aaf275ad8d1b66f742417fba0032ff9d930e84
root <sample@example.com>
2023-04-23 02:03:20 +0900

    test

---
+++ index.search.js
@@ -0,0 +1,23 @@
+var relearn_search_index = [
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
